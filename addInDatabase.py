a5ae35bef10e2ca85c242bcd6d973051600ae434
Asticotboy <c.ludwigs@outlook.fr>
2022-10-12 21:58:38 +0200

    Update

--- addInDatabase.py
+++ addInDatabase.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def connectToDatabase():
     try:
-        conn = sqlite3.connect('database.db')
+        conn = sqlite3.connect('data/database.db')
         return conn
     except:
         print('Error connecting to database')
@@ -14,8 +14,8 @@ def addInDatabase():
     if conn is not None:
         c = conn.cursor()
         c.execute(f'delete from identite')
-        metier = open('metier.txt', 'r', encoding='utf-8').readlines()
-        with open('names.txt', "r+",encoding="utf-8") as f:
+        metier = open('data/metier.txt', 'r', encoding='utf-8').readlines()
+        with open('data/names.txt', "r+",encoding="utf-8") as f:
             i = 1
             for name in f:
                 
---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter
+from BaseDeDonnees import BaseDeDonnees
+
+"""figd_x7RI6RIJEAKFF75aS00IaDkivLUrvAl61IidC_Lx""" #unique figma token
+
+class main(BaseDeDonnees):
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Main")
+        self.root.geometry("300x300")
+        self.root.resizable(False, False)
+        self.root.mainloop()
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    

68bdc9da866c260cb528ae4d8e9c8c89f15d6046
EnderNight <endernightmc@gmail.com>
2022-10-14 08:41:20 +0200

    Update

--- Generator.py
+++ Generator.py
@@ -9,12 +9,16 @@ class Generator :
 
     def __init__(self) :
         pass
+    def naissance(self) :
+        age = randint(20, 80)
+        date = currentDateTime.date()
+        year = date.strftime("%Y")
+
+
     def num_phone(self) :
         id_list = ["06", "07"]
         id_probas = [.70, .30]
         id = choices(id_list, id_probas)
-
-
     def getImage(self):
         f = open("./data/identityImage.jfif",'wb')
         f.write(requests.get('https://thispersondoesnotexist.com/image', headers={'User-Agent': 'My User Agent 1.0'}).content)
@@ -24,3 +28,4 @@ class Generator :
         image = image.resize((102, 102))
         image.save("./data/identityImage.png")
 
+    def security_number(sexe, annee_naissance, mois)
